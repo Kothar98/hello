66838450d3e61f020e7cb3c2681c13129fa882ce
Kothar98 <kothar@huskers.unl.edu>
2020-08-20 22:32:49 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Kothar Al Naamani
+ * Date: 2020/08/20
+ *
+ * A simple hello world program in C
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf ("Hello World!\n");
+
+    return 0;
+ 
+}
